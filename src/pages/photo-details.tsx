ce7aea4447aedc22435285081b75e6c2cc31dab9
JhonatasMJ <jhonatasjhmj@gmail.com>
2026-07-27 11:06:24 -0300

    feat/mock photo-details image

--- src/pages/photo-details.tsx
+++ src/pages/photo-details.tsx
@@ -1,3 +1,5 @@
+import Button from "@/components/button";
+import ImagePreview from "@/components/image-preview";
 import PhotosNavigator from "@/components/photos/photos-navigator";
 import Container from "@/components/ui/container";
 import Skeleton from "@/components/ui/skeleton";
@@ -6,9 +8,19 @@ import type { Photo } from "@/types/https/photo";
 /* import { useParams } from "react-router-dom"; */
 
 export default function PhotoDetails() {
-/*   const { id } = useParams(); */
+  /*   const { id } = useParams(); */
   const isLoadingPhoto = false;
-  const photo = {} as Photo;
+  const photo = {
+    id: "1",
+    title: "Photo 1",
+    imageId: "portrait-tower.png",
+    albums: [
+      {
+        id: "1",
+        title: "Album 1",
+      },
+    ],
+  } as Photo;
   return (
     <Container>
       <header className="flex items-center justify-between gap-8 mb-8">
@@ -19,6 +31,24 @@ export default function PhotoDetails() {
         )}
         <PhotosNavigator />
       </header>
+      <div className="grid grid-cols-[21rem] gap-24">
+        <div className="flex flex-col gap-3">
+          {!isLoadingPhoto ? (
+            <ImagePreview
+              src={`/images/${photo?.imageId}`}
+              title={photo?.title}
+              imageClassName="h-[21rem]"
+            />
+          ) : (
+            <Skeleton className="h-[21rem]" />
+          )}
+          {!isLoadingPhoto ? (
+            <Button variant="destructive">Excluir</Button>
+          ) : (
+            <Skeleton className="w-full h-10" />
+          )}
+        </div>
+      </div>
     </Container>
   );
 }
